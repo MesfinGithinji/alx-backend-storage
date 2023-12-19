c552ec9bf18538d03461ac9639be3a57568b6d59
MesfinGithinji <meshgithinji@gmail.com>
2023-12-20 00:41:04 +0300

    Insert a new document into a collection based on key word arguments

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Insert a new document in a collection based on
+the key word arguments
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """
+    Returns the new _id
+    """
+    new_document = mongo_collection.insert_one(kwargs)
+    return new_document.inserted_id
+
